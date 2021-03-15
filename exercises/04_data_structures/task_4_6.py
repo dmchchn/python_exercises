5d24405c8b6cd5148f20060260690f9a755e6e3e
dmchchn <root@FT0F115.int.mirax.ru>
2021-03-15 23:11:32 +0300

    Сделаны задания 4.1-4.7

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -11,10 +11,16 @@ Outbound Interface    FastEthernet0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-Предупреждение: в разделе 4 тесты можно легко "обмануть" сделав нужный вывод,
-без получения результатов из исходных данных с помощью Python.
-Это не значит, что задание сделано правильно, просто на данном этапе сложно иначе
-проверять результат.
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+# Solution
+
+string1=ospf_route.replace(',','').split()
+string1.remove('via')
+print(
+        'Prefix                {}'.format(string1[0]),'\n'
+        'AD/Metric             {}'.format(string1[1].strip('[]')),'\n'
+        'Next-Hop              {}'.format(string1[2]),'\n'
+        'Last update           {}'.format(string1[3]),'\n'
+        'Outbound Interface    {}'.format(string1[4]),'\n')
